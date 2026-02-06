da8222d7382062ab0824f1e3598375f491f91950
Adarsh Pandey <pandey.adarsh2910@gmail.com>
2026-02-06 20:22:38 +0530

    p2

--- script/HelpConfig.s.sol
+++ script/HelpConfig.s.sol
@@ -6,12 +6,21 @@ contract HelpConfig {
     struct NetworkConfig {
         address priceFeeder;
     }
+    NetworkConfig public activeConfig;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeConfig = getSepoliaPrice();
+        } else {
+            activeConfig = getAnvilPrice();
+        }
+    }
 
     function getSepoliaPrice() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory networkconfig = NetworkConfig({
+        NetworkConfig memory sepoliaconfig = NetworkConfig({
             priceFeeder: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         });
-        return networkconfig;
+        return sepoliaconfig;
     }
 
     function getAnvilPrice() public pure {}
